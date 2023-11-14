09c80eb4f55867ea3a0bc7ca1af64fc1f4beaa3e
Alexandre Vieira <avsilva@credipronto.com.br>
2023-11-14 16:51:16 -0300

    feat: add rizzotto

--- components/api/CadastroCliente.php
+++ components/api/CadastroCliente.php
@@ -55,7 +55,7 @@ class CadastroCliente extends ClientApiTecnuv
             return true;
 
         } catch (\Exception $err) {
-            var_dump($err);
+            var_dump($err->getMessage());
             return false;
         }
     }
